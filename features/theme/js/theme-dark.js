103468e806fcc00eadcdb9420ea95429bdaeaa9b
orel hassid <orelhassid@outlook.com>
2021-09-26 07:30:31 +0300

    Set Env to theme

--- features/theme/js/theme-dark.js
+++ features/theme/js/theme-dark.js
@@ -1,2 +1,3 @@
 console.log("🎁 Theme Dark");
 localStorage.setItem("oh_theme", "dark");
+__console.environment.ThemeStore.setState({ mode: "dark" });
---
+++ features/theme/js/theme-light.js
@@ -0,0 +1,3 @@
+console.log("🎁 Theme Light");
+localStorage.setItem("oh_theme", "light");
+__console.environment.ThemeStore.setState({ mode: "light" });
