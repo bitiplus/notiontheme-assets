a54e729d556f54c6ae9badd1c910b849c2a5a72f
orel hassid <orelhassid@outlook.com>
2021-09-29 18:08:52 +0300

    Apply RTL only for content

--- features/theme/js/theme-dark.js
+++ features/theme/js/theme-dark.js
@@ -1,3 +1,8 @@
 console.log("🎁 Theme Dark");
 localStorage.setItem("oh_theme", "dark");
 __console.environment.ThemeStore.setState({ mode: "dark" });
+
+element.append(
+  `<script src='https://bitiplus.github.io/notiontheme-assets/features/theme/js/theme-${siteSettings.theme}.js' ></script>`,
+  { html: true }
+);
